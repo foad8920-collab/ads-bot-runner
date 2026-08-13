bac92859c973be63910d4e000947189025bcb07e
foad8920-collab <foad8920@gmail.com>
2026-08-13 21:08:16 +0300

    Specify executable path for Chromium browser

--- publisher.js
+++ publisher.js
@@ -586,6 +586,7 @@ async function processOnePost(post) {
 
     const browser = await chromium.launch({
         headless: true,
+        executablePath: '/usr/bin/chromium-browser',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
