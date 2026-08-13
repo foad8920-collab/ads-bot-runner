dfe414370c1e0f69acb2af798791c8d6359d7e57
foad8920-collab <foad8920@gmail.com>
2026-08-13 20:22:42 +0300

    Update publisher.js

--- publisher.js
+++ publisher.js
@@ -593,7 +593,6 @@ async function processOnePost(post) {
 
     const browser = await chromium.launch({
         headless: true,
-        executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH || undefined,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
